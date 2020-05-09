8a438d03bb5795676944d89ed6a5642a4b924c33
Sowmya Padyana <Sowmya_PR01@ad.infosys.com>
2020-05-10 01:49:51 +0530

    Session 6 1st checkin

---
+++ S6/dataloader.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+class TrainSetLoader():
+    def __init__(self, kwargs, batch_size):
+        self.kwargs = kwargs
+        self.batch_size = batch_size
+
+    def data_loader(self, data_type):
+        torch.manual_seed(1)
+        if data_type == "train":
+            data_load = torch.utils.data.DataLoader(
+                datasets.MNIST('../data', train=True, download=True,
+                                transform=transforms.Compose([
+                                    transforms.RandomRotation((-0.7,0.7), fill=(1,)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.1307,), (0.3081,))
+                                ])),
+                batch_size=self.batch_size, shuffle=True, **self.kwargs)
+        else:
+            data_load = torch.utils.data.DataLoader(
+                datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.1307,), (0.3081,))
+                                ])),
+                batch_size=self.batch_size, shuffle=True, **self.kwargs)
+
+        return data_load
---
+++ S6/train.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from tqdm import tqdm
+
+train_acc = []
+train_losses = []
+
+
+def train_model(model, device, train_loader, optimizer, epoch):
+    model.train()
+    pbar = tqdm(train_loader)
+    correct = 0
+    processed = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        y_pred = model(data)
+
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+        pred = y_pred.argmax(dim=1, keepdim=True)
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+
+        # pbar.set_description(desc= f'loss={loss.item()} batch_id={batch_idx}')
+        pbar.set_description(desc= f'loss={loss.item()} batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+        train_acc.append(100*correct/processed)
